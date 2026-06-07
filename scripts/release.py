f881c4b86c3813aa17ebbf9db28d2eaeb1500a70
Liam Gray <lgray95@alumni.cmu.edu>
2026-06-07 12:43:22 -0700

    chore: add LiamDGray to AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -45,6 +45,7 @@ ACP_REGISTRY_MANIFEST = REPO_ROOT / "acp_registry" / "agent.json"
 
 # Auto-extracted from noreply emails + manual overrides
 AUTHOR_MAP = {
+    "lgray95@alumni.cmu.edu": "LiamDGray",
     "yusufalweshdemir@gmail.com": "Dusk1e",
     "804436395@qq.com": "LaPhilosophie",
     "maxmitcham@mac.home": "maxtrigify",
